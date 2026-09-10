579a0eaf83f1077f89b513b7b04e45ba8fb0802b
Mario Perez <mperez@elysiumcoding.com>
2026-09-10 17:21:58 -0600

    Demonstrate external outbox publish replay

--- samples/Pigeon.Messaging.InMemory.Sample/InMemorySampleScenario.cs
+++ samples/Pigeon.Messaging.InMemory.Sample/InMemorySampleScenario.cs
@@ -2,12 +2,15 @@ namespace Pigeon.Messaging.InMemory.Sample
 {
     using System.Collections.Concurrent;
     using Pigeon.Messaging.Consuming.Dispatching;
+    using Pigeon.Messaging.Producing;
 
     internal sealed class InMemorySampleScenario
     {
         private readonly TaskCompletionSource _billingReceived = new(TaskCreationOptions.RunContinuationsAsynchronously);
         private readonly TaskCompletionSource _auditReceived = new(TaskCreationOptions.RunContinuationsAsynchronously);
+        private readonly TaskCompletionSource _externalOutboxReceived = new(TaskCreationOptions.RunContinuationsAsynchronously);
         private readonly TaskCompletionSource<PigeonConsumeEnvelope> _deferredEnvelope = new(TaskCreationOptions.RunContinuationsAsynchronously);
+        private readonly TaskCompletionSource<PigeonPublishEnvelope> _externalOutboxEnvelope = new(TaskCreationOptions.RunContinuationsAsynchronously);
         private readonly ConcurrentQueue<string> _executionEvents = new();
 
         public string OrderId { get; } = Guid.NewGuid().ToString("N");
@@ -27,8 +30,17 @@ namespace Pigeon.Messaging.InMemory.Sample
                 _auditReceived.TrySetResult();
         }
 
+        public void MarkExternalOutbox(string orderId)
+        {
+            if (orderId == OrderId)
+                _externalOutboxReceived.TrySetResult();
+        }
+
         public void CaptureDeferredEnvelope(PigeonConsumeEnvelope envelope)
             => _deferredEnvelope.TrySetResult(envelope);
+
+        public void CaptureExternalOutboxEnvelope(PigeonPublishEnvelope envelope)
+            => _externalOutboxEnvelope.TrySetResult(envelope);
 
         public void RecordExecution(string eventName)
             => _executionEvents.Enqueue(eventName);
@@ -41,7 +53,13 @@ namespace Pigeon.Messaging.InMemory.Sample
         public Task WaitForAuditAsync(CancellationToken cancellationToken)
             => _auditReceived.Task.WaitAsync(cancellationToken);
 
+        public Task WaitForExternalOutboxAsync(CancellationToken cancellationToken)
+            => _externalOutboxReceived.Task.WaitAsync(cancellationToken);
+
         public Task<PigeonConsumeEnvelope> WaitForDeferredEnvelopeAsync(CancellationToken cancellationToken)
             => _deferredEnvelope.Task.WaitAsync(cancellationToken);
+
+        public Task<PigeonPublishEnvelope> WaitForExternalOutboxEnvelopeAsync(CancellationToken cancellationToken)
+            => _externalOutboxEnvelope.Task.WaitAsync(cancellationToken);
     }
 }
--- samples/Pigeon.Messaging.InMemory.Sample/OrderRoutes.cs
+++ samples/Pigeon.Messaging.InMemory.Sample/OrderRoutes.cs
@@ -5,14 +5,19 @@ namespace Pigeon.Messaging.InMemory.Sample
     internal static class OrderRoutes
     {
         public const string CreatedTopic = "orders.created";
+        public const string ExternalOutboxTopic = "orders.external-outbox";
         public const string CreatedVersion = "1.0.0";
         public const string BillingSubscription = "billing-module";
         public const string AuditSubscription = "audit-module";
+        public const string ExternalOutboxSubscription = "external-outbox-module";
 
         public static readonly PigeonRouteKey CreatedForBilling =
             new(CreatedTopic, CreatedVersion, BillingSubscription);
 
         public static readonly PigeonRouteKey CreatedForAudit =
             new(CreatedTopic, CreatedVersion, AuditSubscription);
+
+        public static readonly PigeonRouteKey ExternalOutbox =
+            new(ExternalOutboxTopic, CreatedVersion, ExternalOutboxSubscription);
     }
 }
